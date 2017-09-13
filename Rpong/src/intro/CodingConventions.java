05a60465cfb084f0545c47132c1c7ce88959de17
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-09-13 10:58:05 -0400

    Completed the Coding Conventions lesson

--- Rpong/src/intro/CodingConventions.java
+++ Rpong/src/intro/CodingConventions.java
@@ -13,16 +13,21 @@ public class CodingConventions {
 	that is because the thing it returns is an instance of the class therefore 
 	the name of the constructor must always match the name of the class
 	*/
-	public CodingConventions() {
+	public CodingConventions(String name, int index) {
 		//In Java, fields are instantiated in a constructor
-		name = "Mr.Nockles";
-		description = " is a teacher";
+		//there is a local variable called 'name' and a field called 'name'
+		//the field persists, but the local variable data is destroyed at the end of his method
+		// distinguish the field by using the reserved word "this"
+		this.name = name;
+		//static reference begins w. a class name
+		description = IntroMain.DESCRIPTIONS[index];
 		
 	}
 
 	public void doStuff() {
 		//static method call
-		System.out.println(name+description);
+		System.out.println(name+description); //use a space between your operations.
+		System.out.println("The square root of 12 is " + Math.sqrt(12));
 		
 	}
 	
