81c88d19aa59a38c9e361c979994abea2e8264ad
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-09-29 10:30:40 -0400

    Chatbot project part 1 imported

---
+++ Rpong/src/chatbotProject/Chatbot.java
@@ -0,0 +1,30 @@
+package chatbotProject;
+
+public class Chatbot {
+	
+	private String userName;
+	private Topic ricky;
+	private boolean chatting;
+	
+	public Chatbot() {
+		ricky = new ChatbotRicky();
+		userName = "unknown user";
+		chatting = true;
+	}
+
+	public void startTalking() {
+		ChatbotMain.print("Welcome to our chatbot! What is your name?");
+		userName = ChatbotMain.getInput();
+		while(chatting) {
+			ChatbotMain.print("What do you want to talk about?");
+			String response = ChatbotMain.getInput();
+			if(ricky.isTriggered(response)) {
+				chatting = false;
+				ricky.startChatting();
+			}else {
+				ChatbotMain.print("I'm sorry. I don't understand.");
+			}
+		}
+	}
+
+}
