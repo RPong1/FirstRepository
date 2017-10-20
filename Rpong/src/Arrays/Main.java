2a9cca28d7664f4db1453f769100f8d6524ca11e
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-10-20 11:20:12 -0400

    PUSH 1020

--- Rpong/src/Arrays/Main.java
+++ Rpong/src/Arrays/Main.java
@@ -1,9 +1,50 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
-		ObjectArrays oA = new ObjectArrays();
-	}
+		//ObjectArrays oA = new ObjectArrays();
+		Person s = new Person("Ben", "Nockles", Borough.NY_BOROUGHS[0]);
+		int x = 10;
+		int[] a = {5,6,7};
+		test3(s,x,a);
+		System.out.println(s+", "+x+", "+Arrays.toString(a));
+		}
 
+	/*
+	 PASS BY VALUE EXAMPLES
+	 
+	 //when you have a local variable, you can't change the local variable to where it comes from
+	 
+	 */
+	public static void test(Person s, int x, int[] array) {
+		String name = s.getFirstName();
+		name = "Ilona";
+		x = 5;
+		array = new int[3];
+		array[0] = 1;
+		array[1] = 2;
+		array[2] = 3;
+	}
+	// changes the name and array
+	public static void test2(Person s, int x, int[] array) {
+		s.setFirstName("Ilona");
+		x = 5;
+		array = new int[3]; 
+		array[0] = 1;
+		array[1] = 2;
+		array[2] = 3;
+	}
+	
+	public static void test3(Person s, int x, int[] array) {
+		s.setFirstName("Ilona");
+		x = 5;
+		//array = new int[3]; AS LONG AS YOU DONT MAKE A NEW ARRAY,
+		//THE REFERENCES WILL CHANGE
+		array[0] = 1;
+		array[1] = 2;
+		array[2] = 3;
+	}
 }
--- Rpong/src/Arrays/Person.java
+++ Rpong/src/Arrays/Person.java
@@ -15,6 +15,7 @@ public class Person {
 	private Borough home;
 	private Hobby hobby;
 	private Person[] friends;
+	private String nickname;
 	
 	public Person(String first, String last, Borough home) {
 		this.firstName  = first;
@@ -22,6 +23,7 @@ public class Person {
 		this.home = home;
 		this.hobby = Hobby.randomHobby();
 		friends = new Person[3];
+		this.nickname = createNickname(firstName);
 	}
 	
 	public void mingle(Person[] people) {
@@ -76,7 +78,47 @@ public class Person {
 	
 	
 	public String toString() {
-		return "My name is "+firstName+" "+lastName+" and I live in "+home+". I like "+hobby;
+		return "My name is "+firstName+" "+lastName+" . Call me "+nickname+
+				", and I live in "+home+". I like "+hobby;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+		nickname = createNickname(firstName);
+	}
+
+	//JAVA IS PASS-BY-VALUE
+	//meaning parameters of a method are values and not references so if values get
+	//changed, original object is not affected. In this case, name will not be changed.
+	//NOTHING can change this.firstName via NAME
+	public static String createNickname(String name) {
+		int vowelpsn = findVowel(name);
+		return name.substring(0, vowelpsn);
+	}
+	
+	public static int findVowel(String s) {
+		boolean vowel = false;
+		s = s.toLowerCase();
+		int length = s.length();
+		for(int i = 0; i < s.length(); i++){
+			if(s.substring(i,i+1).equals("a") ||					
+				s.substring(i,i+1).equals("e") ||
+				s.substring(i,i+1).equals("i") ||
+				s.substring(i,i+1).equals("o") ||
+				s.substring(i,i+1).equals("u")) {
+				if(!vowel) {
+					vowel = true;
+				}
+				else {
+					return i;
+				}
+			}
+		}
+		return length;
 	}
 }
 
