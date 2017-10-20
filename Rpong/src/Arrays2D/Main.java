10c97dc5ae0d63c2843b117481d4397bff7d334b
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-10-20 11:45:38 -0400

    1020 PART 3

---
+++ Rpong/src/Arrays2D/Main.java
@@ -0,0 +1,18 @@
+package Arrays2D;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	//THIS idea is avoiding an ArrayIndexOutOfBoundsException
+	//you will see it on quizzes, tests, and ON project 
+	//SO BE MINDFUL!
+	public static void changeNeighbors(int[] array, int psn) {
+		array[psn]++;
+		if(psn > 0){
+			
+		}
+}
