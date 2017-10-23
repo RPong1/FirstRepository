9540f10556c949cfe45dce2bb4221574b02e714e
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-10-23 11:40:49 -0400

    2nd PUSH to caveExplorer

--- Rpong/src/caveExplorer/CaveRoom.java
+++ Rpong/src/caveExplorer/CaveRoom.java
@@ -18,10 +18,38 @@ public class CaveRoom {
 	public static final int SOUTH = 2;
 	public static final int WEST = 3;
 	
-	public CaveRoom() {
-		// TODO Auto-generated constructor stub
+	public CaveRoom(String description) {
+		this.description = description;
+		setDefaultContents(" ");
+		contents = defaultContents;
+		// difference btw dC and c is " c becomes x when you are inside this room.
+		// when you leave, it goes back to dC
+		
+		// by Default, arrays will populate with 'null' meaning there are no connections.
+		borderingRooms = new CaveRoom[4];
+		doors = new Door[4];
+		setDirections();
 	}
 
+	public void setDirections(int[] arr) {
+		directions = "";
+		boolean doorFound = false;
+		for(int i = 0; i < doors.length; i++) {
+			if(doors[i] != null) {
+				doorFound = true;
+				directions += "\n  There is a "+doors[i].getDescription() + " to "+toDirection(i)+". "+doors[i].getDetails();
+			}
+		}
+		if(!doorFound) {
+			directions += "There is no way out. You are stuck.";
+		}
+	}
+
+	public static String toDirection(int dir) {
+		String[] directions = {"the North","the East","the South","the West"};
+		return directions[dir];
+	}
+	
 	public String getDescription() {
 		return description;
 	}
