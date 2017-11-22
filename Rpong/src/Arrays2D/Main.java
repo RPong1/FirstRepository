71186a49e5699ede0fb0f6af0e601504dedb16ea
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-11-22 10:35:27 -0500

    Prey class doesnt work as of now

--- Rpong/src/Arrays2D/Main.java
+++ Rpong/src/Arrays2D/Main.java
@@ -2,17 +2,21 @@ package Arrays2D;
 
 public class Main {
 
+	/*
+	
 	public static void main(String[] args) {
 		
 
 	}
 	
+	*/
+	
 	//THIS idea is avoiding an ArrayIndexOutOfBoundsException
 	//you will see it on quizzes, tests, and ON project 
 	//SO BE MINDFUL!
-	public static void changeNeighbors(int[] array, int psn) {
-		array[psn]++;
-		if(psn > 0){
+	// public static void changeNeighbors(int[] array, int psn) {
+	//	array[psn]++;
+	//	if(psn > 0){
 			
-		}
-}
+	//	}
+
