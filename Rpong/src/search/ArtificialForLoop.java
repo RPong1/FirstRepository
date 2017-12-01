9ac364ace512ff9ea165b235a39e3c3b7f13b025
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-12-01 11:45:03 -0500

    push

--- Rpong/src/search/ArtificialForLoop.java
+++ Rpong/src/search/ArtificialForLoop.java
@@ -2,6 +2,7 @@ package search;
 
 public class ArtificialForLoop {
 
+	/*
 	public static void main(String[] args) {
 		forLoop(10, new Action() {
 			int count = 1;
@@ -18,4 +19,33 @@ public class ArtificialForLoop {
 			forLoop(n - 1, a );
 		}
 	}
+	*/
+	
+	public static void main(String[] args) {
+		int value = 18;
+		//System.out.println(value + "! is equal to "+factorial(value));
+		System.out.println(value + " has " + countPrimeFactors(value,2)+ " prime factors.");
+		
+	}
+	
+	public static int countPrimeFactors(int value, int testPrime) {
+		if(value == 1) {
+			return 0;
+		}
+		else if(value % testPrime == 0) {
+			return countPrimeFactors(value/testPrime, testPrime) + 1;
+		}
+		else {
+			testPrime++;
+			return countPrimeFactors(value,testPrime);
+		}
+	}
+	
+	public static int factorial(int value) {
+		if(value > 1) {
+			value *= factorial(value - 1);
+		}
+		return value;
+	}
+	
 }
