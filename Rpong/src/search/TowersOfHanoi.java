b4ccf8e82ace8bdfe62c3e7bc2fe03d4ad837b78
RPong1 <rpong8254@bths.edu>
2017-12-05 10:23:54 -0500

    Update TowersOfHanoi.java

--- Rpong/src/search/TowersOfHanoi.java
+++ Rpong/src/search/TowersOfHanoi.java
@@ -13,12 +13,4 @@ public class TowersOfHanoi {
 	    	   System.out.println("Put disc "+ start + " to disc " + end);
 	       }
 	   }
-
-	   public static void main(String[] args) {
-	       TowersOfHanoi towersOfHanoi = new TowersOfHanoi();
-	       System.out.print("Enter number of discs: ");
-	       Scanner scanner = new Scanner(System.in);
-	       int discs = scanner.nextInt();
-	       towersOfHanoi.solve(discs, "A", "B", "C");
-	   }
 	}
