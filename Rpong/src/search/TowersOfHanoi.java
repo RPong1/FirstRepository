3634c524012ee682faec167f6d806588993be55d
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-12-05 10:56:25 -0500

    hanoi-ce

--- Rpong/src/search/TowersOfHanoi.java
+++ Rpong/src/search/TowersOfHanoi.java
@@ -1,14 +1,16 @@
 package search;
 
-import java.util.Scanner;
-
 public class TowersOfHanoi {
 
-	   public void solve(int n, String start, String helper, String end) {
+		public static void main(String[] args) {
+			hanoi(3, "A", "B", "C");
+		}
+	
+	   public static void hanoi(int n, String start, String helper, String end) {
 	       if (n != 1) {
-	    	   solve(n - 1, start, end, helper);
+	    	   hanoi(n - 1, start, end, helper);
 	           System.out.println("Put disc "+start + " to disc " + end);
-	           solve(n - 1, helper, start, end);
+	           hanoi(n - 1, helper, start, end);
 	       } else {
 	    	   System.out.println("Put disc "+ start + " to disc " + end);
 	       }
