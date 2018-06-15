6c92c1fd43b56a2174c0e3db70c32e6f91437c88
Ricky <Ricky@DESKTOP-VORJ7C1.home>
2018-06-15 18:28:35 -0400

    did it

--- Rpong/src/Arrays2D/Main.java
+++ Rpong/src/Arrays2D/Main.java
@@ -2,21 +2,32 @@ package Arrays2D;
 
 public class Main {
 
-	/*
-	
 	public static void main(String[] args) {
+		judgeCircle("DURDLDRRLL");
 		
-
 	}
-	
-	*/
-	
-	//THIS idea is avoiding an ArrayIndexOutOfBoundsException
-	//you will see it on quizzes, tests, and ON project 
-	//SO BE MINDFUL!
-	// public static void changeNeighbors(int[] array, int psn) {
-	//	array[psn]++;
-	//	if(psn > 0){
-			
-	//	}
 
+	public static boolean judgeCircle(String moves) {
+        int l = 0;
+        int d = 0;
+        int u = 0;
+        int r = 0;
+        String[] arr = {"UD","LR"};
+        for(int i = 0; i < moves.length(); i++){
+            if(moves.substring(i,i+1).toLowerCase().equals("l")){
+                l++;
+            }
+            if(moves.substring(i,i+1).toLowerCase().equals("d")){
+                d++;
+            }
+            if(moves.substring(i,i+1).toLowerCase().equals("u")){
+                u++;
+            }
+            if(moves.substring(i,i+1).toLowerCase().equals("r")){
+                r++;
+            }   
+        }
+        System.out.println("L: "+ l + " R: " + r + " D: " + d + " U: " + u);
+        return ((l == r) && (d == u));
+    }
+}
